94a28e3f21b0d201e780090ac5aa7781b5022aa4
Iqra khan <102247457+Iqrakhan22@users.noreply.github.com>
2023-08-04 18:59:14 +0530

    Update _5CountNoOfSubsetWGivenDiff.java

--- _5CountNoOfSubsetWGivenDiff.java
+++ _5CountNoOfSubsetWGivenDiff.java
@@ -34,6 +34,17 @@ public class _5CountNoOfSubsetWGivenDiff {
         int[][] t = new int[size+1][sum+1];
         System.out.println(countSubsetSumDP(arr, t, sum ,size));
 
+    }
+    public int countKDifference(int[] nums, int k) {
+        int count =0;
 
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if((Math.abs(nums[i]-nums[j])) == k){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
